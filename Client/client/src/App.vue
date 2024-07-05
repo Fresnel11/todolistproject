3114dc97abea6a62108569c4924a42b29a27d61d
Fresnel Jean Claude Cossou <jean.cossou@chem.loc>
2024-07-05 08:57:14 +0100

    ma sauvegarde

--- Client/client/src/App.vue
+++ Client/client/src/App.vue
@@ -27,7 +27,14 @@
             <tbody>
               <tr>
                 <td>
-                  <input type="checkbox">
+                  <div class="checkbox-wrapper-31">
+                      <input type="checkbox"/>
+                    <svg viewBox="0 0 35.6 35.6">
+                      <circle class="background" cx="17.8" cy="17.8" r="17.8"></circle>
+                      <circle class="stroke" cx="17.8" cy="17.8" r="14.37"></circle>
+                      <polyline class="check" points="11.78 18.12 15.55 22.23 25.17 12.87"></polyline>
+                    </svg>
+                  </div>
                 </td>
                 <td>todo text</td>
                 <td>Etat</td>
@@ -182,6 +189,66 @@ table {
   box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
 }
 
+.checkbox-wrapper-31:hover .check {
+    stroke-dashoffset: 0;
+  }
+
+  .checkbox-wrapper-31 {
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+  }
+  .checkbox-wrapper-31 .background {
+    fill: #ccc;
+    transition: ease all 0.6s;
+    -webkit-transition: ease all 0.6s;
+  }
+  .checkbox-wrapper-31 .stroke {
+    fill: none;
+    stroke: #fff;
+    stroke-miterlimit: 10;
+    stroke-width: 2px;
+    stroke-dashoffset: 100;
+    stroke-dasharray: 100;
+    transition: ease all 0.6s;
+    -webkit-transition: ease all 0.6s;
+  }
+  .checkbox-wrapper-31 .check {
+    fill: none;
+    stroke: #fff;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-width: 2px;
+    stroke-dashoffset: 22;
+    stroke-dasharray: 22;
+    transition: ease all 0.6s;
+    -webkit-transition: ease all 0.6s;
+  }
+  .checkbox-wrapper-31 input[type=checkbox] {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    margin: 0;
+    opacity: 0;
+    -appearance: none;
+  }
+  .checkbox-wrapper-31 input[type=checkbox]:hover {
+    cursor: pointer;
+  }
+  .checkbox-wrapper-31 input[type=checkbox]:checked + svg .background {
+    fill: #6cbe45;
+  }
+  .checkbox-wrapper-31 input[type=checkbox]:checked + svg .stroke {
+    stroke-dashoffset: 0;
+  }
+  .checkbox-wrapper-31 input[type=checkbox]:checked + svg .check {
+    stroke-dashoffset: 0;
+  }
+
+
 .icons {
   
   display: flex;
@@ -189,10 +256,23 @@ table {
 }
 
 .icon1 {
+  transition: transform 0.2s;
   cursor: pointer;
 }
 
 .icon2 {
+  transition: transform 0.1s;
   cursor: pointer;
 }
+
+.icon1:hover {
+  transform: scale(1.1);
+}
+
+.icon2:hover {
+  transform: scale(1.1);
+  
+}
+
+
 </style>
